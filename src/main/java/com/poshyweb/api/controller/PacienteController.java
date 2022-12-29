7de77c878e8c8db585e02604f6e14b3a8f1e3237
Roberto <endriosrobert@gmail.com>
2022-12-29 14:14:32 -0500

    criação da classe de controller MedicoController com dois methodos o de lsita todos e o inset que vai fazer o post.

--- src/main/java/com/poshyweb/api/controller/PacienteController.java
+++ src/main/java/com/poshyweb/api/controller/PacienteController.java
@@ -1,17 +1,29 @@
 package com.poshyweb.api.controller;
 
+import com.poshyweb.api.dominio.dto.PacienteDTO;
 import com.poshyweb.api.dominio.entity.PacienteEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.poshyweb.api.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/pacientes")
 public class PacienteController {
-    @PostMapping(value = "/cadastrar")
-    public void cadastrar(@RequestBody PacienteEntity dados) {
-        System.out.println("dados recebido: " +dados);
+    @Autowired
+    private PacienteService service;
+
+    @PostMapping(value = "/insert")
+    public PacienteEntity cadastrar(@RequestBody PacienteDTO pacienteDTO) {
+        PacienteEntity pacienteEntity = service.savePaciente(pacienteDTO);
+        return pacienteEntity;
+    }
+
+    @GetMapping(value = "/listAll")
+    public List<PacienteEntity> listAllPacientes() {
+        List<PacienteEntity> listAllPaciente = service.listAll();
+        return listAllPaciente;
     }
 
 }
