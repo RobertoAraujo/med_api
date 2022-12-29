7890f8a778e04a36a70638b92afe1ad55f440dde
Roberto <endriosrobert@gmail.com>
2022-12-29 14:18:32 -0500

    criação da classe entidade EnderecoEntity

--- src/main/java/com/poshyweb/api/dominio/entity/EnderecoEntity.java
+++ src/main/java/com/poshyweb/api/dominio/entity/EnderecoEntity.java
@@ -1,5 +1,31 @@
 package com.poshyweb.api.dominio.entity;
 
-public record EnderecoEntity (String longradouro,String cep, String cidade, String uf,  String numero, String complemento){
+import com.poshyweb.api.dominio.dto.EnderecoDTO;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoEntity {
+    private String longradouro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String numero;
+    private String complemento;
+
+    public EnderecoEntity(EnderecoDTO enderecoDTO) {
+        this.longradouro = enderecoDTO.longradouro();
+        this.cep = enderecoDTO.cep();
+        this.cidade = enderecoDTO.cidade();
+        this.uf = enderecoDTO.uf();
+        this.numero = enderecoDTO.numero();
+        this.complemento = enderecoDTO.complemento();
+    }
 }
