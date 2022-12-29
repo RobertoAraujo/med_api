b326164638bb34db8e5841867ca8abcae3104045
Roberto <endriosrobert@gmail.com>
2022-12-29 14:19:25 -0500

    criação da classe PacienteEntity

--- src/main/java/com/poshyweb/api/dominio/entity/PacienteEntity.java
+++ src/main/java/com/poshyweb/api/dominio/entity/PacienteEntity.java
@@ -1,4 +1,32 @@
 package com.poshyweb.api.dominio.entity;
 
-public record PacienteEntity (String nome, String email, String telefone, String cpf, EnderecoEntity endereco){
+import com.poshyweb.api.dominio.dto.PacienteDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "paciente")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class PacienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    @Embedded
+    private EnderecoEntity endereco;
+
+    public PacienteEntity(PacienteDTO pacienteDTO) {
+        this.nome = pacienteDTO.nome();
+        this.email = pacienteDTO.email();
+        this.telefone = pacienteDTO.telefone();
+        this.cpf = pacienteDTO.cpf();
+        this.endereco = new EnderecoEntity(pacienteDTO.endereco());
+    }
 }
