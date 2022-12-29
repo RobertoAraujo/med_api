f40fe0f7feeb4c894d3c3cb8f0464f82b0e14aa3
Roberto <endriosrobert@gmail.com>
2022-12-29 14:11:54 -0500

    criação da classe MedicoEntity

--- src/main/java/com/poshyweb/api/dominio/entity/MedicoEntity.java
+++ src/main/java/com/poshyweb/api/dominio/entity/MedicoEntity.java
@@ -1,6 +1,35 @@
 package com.poshyweb.api.dominio.entity;
 
 import com.poshyweb.api.constants.Expecializacao;
+import com.poshyweb.api.dominio.dto.MedicoDTO;
+import jakarta.persistence.*;
+import lombok.*;
 
-public record MedicoEntity (String nome, String email, String telefone, Expecializacao expecializacao, String crm, EnderecoEntity endereco){
+@Entity
+@Table(name = "medico")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class MedicoEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    @Enumerated(EnumType.STRING)
+    private Expecializacao expecializacao;
+    private String crm;
+    @Embedded
+    private EnderecoEntity endereco;
+
+    public MedicoEntity(MedicoDTO medicoDTO) {
+        this.nome = medicoDTO.nome();
+        this.email = medicoDTO.email();
+        this.telefone = medicoDTO.telefone();
+        this.expecializacao = medicoDTO.expecializacao();
+        this.crm = medicoDTO.crm();
+        this.endereco = new EnderecoEntity(medicoDTO.endereco());
+    }
 }
