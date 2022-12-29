9b52d7a6c0f6b8dd1329453e501aeaa390670cb7
Roberto <endriosrobert@gmail.com>
2022-12-29 14:12:35 -0500

    criação da interface MedicoRepository

--- src/main/java/com/poshyweb/api/dominio/repository/MedicoRepository.java
+++ src/main/java/com/poshyweb/api/dominio/repository/MedicoRepository.java
@@ -1,4 +1,14 @@
 package com.poshyweb.api.dominio.repository;
 
-public interface MedicoRepository {
+import com.poshyweb.api.dominio.dto.MedicoDTO;
+import com.poshyweb.api.dominio.entity.MedicoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<MedicoEntity, Long> {
+    List<MedicoEntity> findAll();
 }
