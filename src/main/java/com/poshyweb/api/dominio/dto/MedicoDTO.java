69b5bad2aa3912d132453fa3136b51475f8d6cb1
Roberto <endriosrobert@gmail.com>
2023-01-02 17:20:54 -0500

    atotaçães do ben validation

--- src/main/java/com/poshyweb/api/dominio/dto/MedicoDTO.java
+++ src/main/java/com/poshyweb/api/dominio/dto/MedicoDTO.java
@@ -1,6 +1,22 @@
 package com.poshyweb.api.dominio.dto;
 
 import com.poshyweb.api.constants.Expecializacao;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record MedicoDTO(String nome, String email, String telefone, Expecializacao expecializacao, String crm, EnderecoDTO endereco){
+public record MedicoDTO(
+        @NotBlank
+        String nome,
+        @NotBlank @Email
+        String email,
+        String telefone,
+        @NotNull
+        Expecializacao expecializacao,
+        @NotBlank @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull @Valid
+        EnderecoDTO endereco) {
 }
